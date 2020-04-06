fc3c1a318b4f5dc8278f3aef5ef788bb579ba837
austinherron <59572868+austinherron@users.noreply.github.com>
2020-04-05 19:06:54 -0600

    Add files via upload

---
+++ array.py
@@ -0,0 +1,19 @@
+name = ["Austin Herron"]
+print (name)
+cars = ['Ford','Chrysler', 'Dodge', 'Ram', 'Jeep', 'Chevy', 'GMC']
+print(cars)
+print(len(cars))
+cars.append ('Buick')
+print(cars)
+print(cars [3])
+cars.insert (2, 'Toyota')
+print(cars)
+cars.pop(4)
+print(cars)
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+my_array_length = (len(cars))
+array_string = 'The length of my array is'
+print(array_string, my_array_length)
